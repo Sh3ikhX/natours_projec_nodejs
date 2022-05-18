8fb09fc78ac94dc60ed1e82689c2d73d36564923
$h3!khX <61138639+Sh3ikhX@users.noreply.github.com>
2022-05-18 20:28:32 +0500

    1st api tours

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
 const port = 3000;
 
-app.get('/', async (req, res) => {
-  res.status(200).json({ message: 'OKAY', app: 'Natours' });
-});
-app.post('/', async (req, res) => {
-  res.status(200).json({ message: 'You can post here...' });
+// app.get('/', async (req, res) => {
+//   res.status(200).json({ message: 'OKAY', app: 'Natours' });
+// });
+// app.post('/', async (req, res) => {
+//   res.status(200).json({ message: 'You can post here...' });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'sucess',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 app.listen(port, () => {
